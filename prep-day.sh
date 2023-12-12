71a0f1d72b470d3df5e7f9dc7645b12daac1568d
Bjoern Doebel <doebel@amazon.de>
2023-12-12 08:04:22 +0100

    prep-day: use GNU sed on MacOS

--- prep-day.sh
+++ prep-day.sh
@@ -68,8 +68,14 @@ impl DaySolution for Day$1 {
 }
 EOF
 
-sed -i "s|// MOD_MARKER|mod day$1;\nuse day$1::Day$1;\n// MOD_MARKER|" src/main.rs
-sed -i "s|        // DAY_MARKER|        $1 => Box::new(Day$1::from_lines(lines)),\n        // DAY_MARKER|" src/main.rs
+SED="sed"
+if [ "$(uname -s)" = "Darwin" ]
+then
+  SED=gsed
+fi
+
+"$SED" -i "s|// MOD_MARKER|mod day$1;\nuse day$1::Day$1;\n// MOD_MARKER|" src/main.rs
+"$SED" -i "s|        // DAY_MARKER|        $1 => Box::new(Day$1::from_lines(lines)),\n        // DAY_MARKER|" src/main.rs
 
 echo "Updated main.rs:"
   git diff src/main.rs
