e9e4d1ede6a837b7cf941a708d43830bd75776c2
Bjoern Doebel <bjoern.doebel@gmail.com>
2023-12-17 14:25:30 +0100

    Day12, part 2 with caching

--- src/day12.rs
+++ src/day12.rs
@@ -1,6 +1,6 @@
-use std::usize;
-
 use crate::{DaySolution, FromInput};
+use cached::proc_macro::cached;
+use itertools::{Itertools, PeekingNext};
 
 // TODO: Model the problem into this struct
 
@@ -14,10 +14,10 @@ pub struct Day12 {
 }
 
 impl FromInput for Day12 {
-    fn from_lines(lines: impl Iterator<Item = String>) -> Self {
+    fn from_lines(_lines: impl Iterator<Item = String>) -> Self {
         let mut groups = vec![];
 
-        for l in lines
+        for l in &_lines.collect_vec()
             // [
             //     "???.### 1,1,3",
             //     ".??..??...?##. 1,1,3",
@@ -33,7 +33,15 @@ impl FromInput for Day12 {
             for (i, item) in l.split_whitespace().enumerate() {
                 match i {
                     0 => {
-                        items = item.chars().collect();
+                        let mut lastc = 'X';
+                        for ch in item.chars().collect::<Vec<char>>() {
+                            if ch == '.' && lastc != '.' {
+                                items.push(ch);
+                            } else if ch != '.' {
+                                items.push(ch);
+                            }
+                            lastc = ch;
+                        }
                     },
                     1 => {
                         numbers = item.split(',').map(|x| str::parse(x).unwrap()).collect();
@@ -51,41 +59,125 @@ impl FromInput for Day12 {
     }
 }
 
-fn is_valid(items: &[char], numbers: &[usize]) -> bool {
-    let s: String = items.iter().collect();
-    let counts: Vec<usize> = s.replace('.', " ").split_whitespace().map(|x| x.len()).collect();
-
-    //println!("{:?} == {:?} {}", counts, numbers, &counts==numbers);
-
-    counts == numbers
+fn compute_schema(items: &Vec<char>) -> Vec<usize>
+{
+    items.into_iter().collect::<String>().replace(".", " ").split_whitespace().map(|x| x.len()).collect::<Vec<usize>>()
 }
 
-fn solve_one(items: &[char], numeric: &[usize]) -> usize {
-    //println!("{:?} | {:?}", items, numeric);
+fn validate(items: &Vec<char>, schema: &Vec<usize>) -> bool {
+    if items.contains(&'?') { return false; }
+    compute_schema(items) == *schema
+}
+
+
+#[cached]
+fn solve_one(items: Vec<char>, schema: Vec<usize>, depth: usize) -> usize {
+    // for i in 0..depth*3 { print!(" "); }
+    // println!("==== {:?} {:?}", items, schema);
+    
     if !items.contains(&'?') {
-        if is_valid(items, numeric) { return 1; }
-        else { return 0; }
+        if validate(&items, &schema) {
+            // for i in 0..depth*3 { print!(" "); }
+            // println!("{:?} {:?} 1", items, schema);
+            return 1;
+        }
+        else {
+            // for i in 0..depth*3 { print!(" "); }
+            // println!("{:?} {:?} 1", items, schema);
+            return 0;
+        }
     }
 
-    let next_quest = items.iter().position(|x| *x == '?').unwrap();
-    solve_one(&[ &items[0..next_quest], vec!['.'].as_slice(), &items[next_quest+1..] ].concat(), numeric)
-     + solve_one(&[ &items[0..next_quest], vec!['#'].as_slice(), &items[next_quest+1..] ].concat(), numeric)
+    /* Empty schema - we must not have any hashes left */
+    if schema.len() == 0 {
+        if items.contains(&'#') { return 0; }
+        return 1;
+    }
+
+    if items.len() == 0 { return 0; }
+
+    let mut idx  = 0_usize;
+
+    match items[idx] {
+        '.' => {
+            return solve_one(items[1..].to_vec(), schema, depth+1);
+        },
+        '#' => {
+            let mut needed_hashes = schema[0];
+
+            // walk the items list. consume # and ? as hashes until we
+            // have found our next group
+            while idx < items.len() && needed_hashes > 0 {
+                if items[idx] == '#' || items[idx] == '?' {
+                    idx += 1;
+                    needed_hashes -= 1;
+                } else if items[idx] == '.' {
+                    break;
+                }
+            }
+
+            // for i in 0..depth*3 { print!(" "); }
+            // println!("idx {} needed_hashes {} len {}", idx, needed_hashes, items.len());
+
+            if needed_hashes > 0 { return 0; }
+
+            if idx == items.len() {
+                if schema.len() == 1 { return 1; } // we matched the last group
+                return 0;
+            }
+
+            // We found a group! Now if the next char is still a hash, we mismatched. If it's a . or a ?, treat it as a .
+            if items[idx] == '#' { return 0; }
+
+            let new_schema = schema[1..].to_vec();
+            return solve_one(items[idx+1..].to_vec(), new_schema, depth+1);
+        },
+        '?' => {
+            let new_items = [vec!['#'], items[1..].to_vec()].concat();
+            return solve_one(new_items, schema.clone(), depth+1) + solve_one(items[1..].to_vec(), schema, depth+1);
+        }
+        _ => { panic!("Invalid item"); }
+    }
 }
 
 impl DaySolution for Day12 {
     fn part_one(&self) -> String {
         let mut sum = 0_usize;
-        for (i, g) in self.groups.iter().enumerate() {
-            sum += solve_one(&g.items, &g.numeric);
-            if i % 50 == 0 {
-                println!("{}/{}", i, self.groups.len());
-            }
+        for (_i, g) in self.groups.iter().enumerate() {
+            // println!("------------------------------------------------------------");
+            let res = solve_one(g.items.clone(), g.numeric.clone(), 0);
+            // println!("{:?} {:?} {}", g.items, g.numeric, res);
+            sum += res;
+            // if _i % 50 == 49 {
+            //     println!("{}/{}", _i+1, self.groups.len());
+            // }
         }
 
         sum.to_string()
     }
 
     fn part_two(&self) -> String {
-        todo!("Solve part two of day 12 using your parsed input")
+        let mut sum = 0_usize;
+        for (_i, g) in self.groups.iter().enumerate() {
+            let mut expanded_items = g.items.clone();
+            expanded_items.push('?');
+            expanded_items.append(&mut g.items.clone());
+            expanded_items.push('?');
+            expanded_items.append(&mut g.items.clone());
+            expanded_items.push('?');
+            expanded_items.append(&mut g.items.clone());
+            expanded_items.push('?');
+            expanded_items.append(&mut g.items.clone());
+
+            let expanded_schema = g.numeric.repeat(5);
+
+            let res = solve_one(expanded_items, expanded_schema, 0);
+            // println!("{:?} {:?} {}", g.items, g.numeric, res);
+            sum += res;
+            if _i % 50 == 49 {
+                println!("{}/{}", _i+1, self.groups.len());
+            }
+        }
+        sum.to_string()
     }
 }
